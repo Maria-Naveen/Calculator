4eed1bb4121374513785c3fb5de1d6862cd155f9
Maria Naveen <marianaveen15@outlook.com>
2023-10-10 18:54:07 +0530

    Aligned the main flex container to the middle

--- script.js
+++ script.js
@@ -1,43 +1,67 @@
-let string = ""; 
+let string = "";
+let inputField = document.querySelector('input');
 let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click',(e)=>{
-        if(e.target.innerHTML == "="){
-            string = eval(string);
-            document.querySelector('input').value = string;
 
-        }
-        else if(e.target.innerHTML == "AC"){
-            string = "";
-            document.querySelector('input').value = string;
-        }
-        else if(e.target.innerHTML == "DEL"){
-            string = string.slice(0,string.length - 1);
-            document.querySelector('input').value = string;
-        }
-        else if(e.target.innerHTML == "%"){
-            string = string/100;
-            document.querySelector('input').value = string;
-        }
-        else{
-            console.log(e.target)
-            string += e.target.innerHTML;
-            document.querySelector('input').value = string;
-        }
+function updateInputField() {
+    inputField.value = string;
+}
 
-    })
-})
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener('keydown',(event)=>{
-        let keyName = event.key;
-        if(keyName == "="){
+function handleButtonClick(e) {
+    let buttonValue = e.target.innerHTML;
+
+    if (buttonValue === "=") {
+        try {
             string = eval(string);
-            document.querySelector('input').value = string;
+        } catch (error) {
+            string = "Error";
         }
-        else if(keyName == "A"){
-            string = ""
-            document.querySelector('input').value = string;
+    } else if (buttonValue === "AC") {
+        string = "";
+    } else if (buttonValue === "DEL") {
+        string = string.slice(0, string.length - 1);
+    } else if (buttonValue === "%") {
+        string = string / 100;
+    } else {
+        string += buttonValue;
+    }
+
+    updateInputField();
+}
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', handleButtonClick);
+});
+
+
+function handleKeyboardEvent(event) {
+    let keyName = event.key;
+
+    if (keyName === "=" || event.keyCode === 13) {
+        event.preventDefault();
+        try {
+            string = eval(string);
+        } catch (error) {
+            string = "Error";
         }
-        
-    })
-})
+    } 
+    else if(event.keyCode === 16 || event.keyCode === 17  || event.keyCode === 18){
+        event.preventDefault();
+    }
+    else if (keyName === "a") {
+        string = "";
+    } else if (keyName === "d" || event.keyCode === 8) {
+        event.preventDefault(); 
+        string = string.slice(0, string.length - 1);
+    } else if (keyName === "%") {
+        string = string / 100;
+    } else {
+        if (!keyName.match(/[0-9]*[\+\-\*\/\.]*[0-9]*/)) {
+            alert("Enter a valid expression");
+        } else {
+            string += keyName;
+        }
+    }
+
+    updateInputField();
+}
+document.addEventListener('keydown', handleKeyboardEvent);
