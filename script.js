1a7159b36912317386db40471a154202b3be1f2c
Maria Naveen <marianaveen15@outlook.com>
2023-10-16 17:54:48 +0530

    git commit all

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let string = "";
 let inputField = document.querySelector('input');
 let buttons = document.querySelectorAll('.button');
+let historyTextArea = document.querySelector('textarea');
+let deleteHistoryButton = document.createElement('button'); // Create a Delete History button
+
+// Initialize history array
+let history = [];
 
 function updateInputField() {
     inputField.value = string;
+}
+
+function updateHistoryTextarea() {
+    historyTextArea.value = history.join('\n');
+    localStorage.setItem('calculatorHistory', JSON.stringify(history));
 }
 
 function handleButtonClick(e) {
@@ -11,7 +21,9 @@ function handleButtonClick(e) {
 
     if (buttonValue === "=") {
         try {
-            string = eval(string);
+            const result = eval(string);
+            history.push(`${string} = ${result}`);
+            string = result;
         } catch (error) {
             string = "Error";
         }
@@ -26,12 +38,12 @@ function handleButtonClick(e) {
     }
 
     updateInputField();
+    updateHistoryTextarea();
 }
 
 Array.from(buttons).forEach((button) => {
     button.addEventListener('click', handleButtonClick);
 });
-
 
 function handleKeyboardEvent(event) {
     let keyName = event.key;
@@ -39,18 +51,18 @@ function handleKeyboardEvent(event) {
     if (keyName === "=" || event.keyCode === 13) {
         event.preventDefault();
         try {
-            string = eval(string);
+            const result = eval(string);
+            history.push(`${string} = ${result}`);
+            string = result;
         } catch (error) {
             string = "Error";
         }
-    } 
-    else if(event.keyCode === 16 || event.keyCode === 17  || event.keyCode === 18){
+    } else if (event.keyCode === 16 || event.keyCode === 17 || event.keyCode === 18) {
         event.preventDefault();
-    }
-    else if (keyName === "a") {
+    } else if (keyName === "a") {
         string = "";
     } else if (keyName === "d" || event.keyCode === 8) {
-        event.preventDefault(); 
+        event.preventDefault();
         string = string.slice(0, string.length - 1);
     } else if (keyName === "%") {
         string = string / 100;
@@ -63,5 +75,22 @@ function handleKeyboardEvent(event) {
     }
 
     updateInputField();
+    updateHistoryTextarea();
 }
+
+// Load history from local storage
+history = JSON.parse(localStorage.getItem('calculatorHistory')) || [];
+
+// Create a "Delete History" button
+deleteHistoryButton.textContent = 'Delete History';
+deleteHistoryButton.addEventListener('click', function () {
+    history = [];
+    updateHistoryTextarea();
+    localStorage.removeItem('calculatorHistory');
+});
+
+// Append the Delete History button to the second container
+document.querySelector('.second.container').appendChild(deleteHistoryButton);
+
 document.addEventListener('keydown', handleKeyboardEvent);
+updateHistoryTextarea(); // Update history textarea on page load
